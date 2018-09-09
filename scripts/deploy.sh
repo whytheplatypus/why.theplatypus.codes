b560ec4c27e7f0169b7d7782e29ce7bfb466f04b
whytheplatypus <whytheplatypus@gmail.com>
2018-09-09 11:45:20 -0400

    Two step deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-git config --global user.email "$GH_EMAIL" 
-git config --global user.name "$CIRCLE_USERNAME"
-git init
-git remote add --fetch origin "$PAGES_REMOTE"
-git add -A
+git add .
 git commit -m "Deploy to GitHub pages"
 git push origin master
